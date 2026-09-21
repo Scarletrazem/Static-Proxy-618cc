a6705d4cb39c3abdcf6ecf66d04421d11553e762
Scarletrazem <Humanitysucks356@outlook.com>
2026-09-21 13:27:01 -0400

    Cookie patch.. might work.

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
 importScripts('./uv/uv.sw.js');
 
+// Auto-inject cookie credentials to prevent the black redirect screen loop
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
+
 const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', event =>
+self.addEventListener('fetch', event => {
     event.respondWith(
         sw.fetch(event)
     )
-);
+});
